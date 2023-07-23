312d1185674dcd65d158840cd34be28ac4f12f31
Al - Amin <alaminhimu0@gmail.com>
2023-07-24 01:41:07 +0600

    item by id api created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
@@ -15,9 +16,6 @@ app.use(cors(corsConfig));
 app.options("", cors(corsConfig));
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kacof5g.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,6 +32,22 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+
+    const collegeCollection = client.db("collegeBookingDB").collection("colleges")
+
+    app.get('/colleges', async(req, res) => {
+      const result = await collegeCollection.find().toArray();
+      res.send(result)
+    });
+
+    app.get('/colleges/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: id };
+      const result = await collegeCollection.findOne(query);
+      res.send(result);
+    })
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
@@ -47,7 +61,7 @@ run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
-    res.send("surver is running");
+    res.send("server is running");
   });
   
   app.listen(port, () => {
