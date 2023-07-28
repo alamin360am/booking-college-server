b9c3c3f3b0eb5b7cc668de4531c4f04eb8508c0a
Al - Amin <alaminhimu0@gmail.com>
2023-07-28 17:05:44 +0600

    delete api created

--- index.js
+++ index.js
@@ -72,6 +72,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/admission/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await admissionCollection.deleteOne(query);
+      res.send(result);
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
